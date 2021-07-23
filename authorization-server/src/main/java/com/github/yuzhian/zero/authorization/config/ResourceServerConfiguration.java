ea123c7a53b515137e9bf178a77e272c4da2d8a0
yuzhian <yu97@live.com>
2021-07-23 23:58:19 +0800

    配置资源作用域

--- authorization-server/src/main/java/com/github/yuzhian/zero/authorization/config/ResourceServerConfiguration.java
+++ authorization-server/src/main/java/com/github/yuzhian/zero/authorization/config/ResourceServerConfiguration.java
@@ -2,7 +2,9 @@ package com.github.yuzhian.zero.authorization.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 /**
  * 资源服务配置
@@ -10,5 +12,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @Configuration
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class ResourceServerConfiguration {
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/profile/**").access("#oauth2.hasScope('Profile')");
+    }
 }
